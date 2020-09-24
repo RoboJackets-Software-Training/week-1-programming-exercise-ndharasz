699680d12faab4d9dee7ad02257feb7878523122
Noah Harasz <ndharasz@live.com>
2020-09-24 20:06:35 +0000

    complete project 1.3

--- project_1_3/project_1_3.cpp
+++ project_1_3/project_1_3.cpp
@@ -7,16 +7,27 @@ std::vector<double> readInVector(std::string s) {
   std::vector<double> result;
   while(s.find(',', prev_location) != std::string::npos) {
     next_location = s.find(',', prev_location);
-    //std::cout << "prev_location: " << prev_location << std::endl;
-    //std::cout << "next_location: " << next_location << std::endl;
-    // substr [,]
-    //std::cout << s.substr(prev_location, next_location - prev_location) << std::endl;
+    // std::cout << "prev_location: " << prev_location << std::endl;
+    // std::cout << "next_location: " << next_location << std::endl;
+    // // substr [,]
+    // std::cout << s.substr(prev_location, next_location - prev_location) << std::endl;
     result.push_back(std::stod(s.substr(prev_location, next_location - prev_location)));
     next_location++;
     prev_location = next_location;
   }
   result.push_back(std::stod(s.substr(prev_location, std::string::npos)));
   return result;
+}
+
+void printVector(std::string name, std::vector<double> v) {
+  std::cout << name << ": {";
+  for (int i = 0; i < v.size(); i++) {
+    std::cout << " " << v[i];
+    if (i < v.size() - 1) {
+      std::cout << ",";
+    }
+  }
+  std::cout << " }" << std::endl;
 }
 
 int main() {
@@ -38,8 +49,40 @@ int main() {
   // TODO write your code here
   // =========== START =========
 
+  printVector("x", x);
+  printVector("w", w);
 
+  int pad_size = w.size()/2;
+  std::vector<double> padded(x.size() + 2 * pad_size);
+  for (int i = 0; i < padded.size(); i++) {
+    if (i < pad_size) {
+      if (pack_with_zeros) {
+        padded[i] = 0;
+      } else {
+        padded[i] = x[0];
+      }
+    } else if (i > padded.size() - 1 - pad_size) {
+      if (pack_with_zeros) {
+        padded[i] = 0;
+      } else {
+        padded[i] = x.back();
+      }
+    } else {
+      padded[i] = x[i - pad_size];
+    }
+  }
 
+  // printVector("padded", padded);
+
+  for (int i = 0; i <= (padded.size() - w.size()); i++) {
+    double val = 0;
+    for (int j = 0; j < w.size(); j++) {
+      val += padded[i + j] * w[j];
+    }
+    y.push_back(val);
+  }
+
+  printVector("y", y);
 
   // =========== END ===========
 
